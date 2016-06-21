2cd15bce90fda87f1e74fcbdacf84c71557eed2c
Aaron Porter <aaron.w.porter@gmail.com>
2016-06-21 14:03:24 -0700

    use user from users array to append streamStatus, move userURL to left side of viewer

--- js/index.js
+++ js/index.js
@@ -7,15 +7,15 @@ $(document).ready(function() {
     // get basic user information
     $.getJSON("https://api.twitch.tv/kraken/users/" + user + "?callback=?", function(userData) {
       console.log(userData);
-        $(".viewer-container").append('<div class="user" id="' + userData.display_name + '"><img class="logo" src="' + userData.logo + '"/><a class="userURL" href="' + userData._links.self + '"><h3 class="userName">' + userData.display_name + '</h3></a></div>');
+        $(".viewer-container").append('<div class="user" id="' + user + '"><img class="logo" src="' + userData.logo + '"/><a class="userURL" href="' + userData._links.self + '"><h3 class="userName">' + userData.display_name + '</h3></a></div>');
 
         // get the stream information
         $.getJSON('https://api.twitch.tv/kraken/streams/' + user + '?callback=?', function(streamData) {
           console.log(streamData);
           if (streamData.stream === null) {
-            $("#" + user).append('<h3 class="streamStatus">Offline</h3>');
+            $('#' + user).append('<h3 class="streamStatus">Offline</h3>');
           } else {
-            $("#" + user).append('<h3 class="streamStatus">' + streamData.stream.channel.game + ': ' + streamData.stream.channel.status.slice(0,40) + '...</h3>');
+            $('#' + user).append('<h3 class="streamStatus">' + streamData.stream.channel.game + ': ' + streamData.stream.channel.status.slice(0,40) + '...</h3>');
           }
         });
       }
